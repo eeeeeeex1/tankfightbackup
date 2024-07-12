bcb461001e53c18e4593c369d3a615e1a9d4b37f
Ismweirdo <2074437070@qq.com>
2024-07-12 09:45:18 +0800

    lmk

---
+++ TK2/temp/programming/packer-driver/targets/editor/chunks/6c/6cf21e81582232dd2446393c126b03c349a6ed17.js
@@ -0,0 +1,86 @@
+System.register(["__unresolved_0", "cc", "__unresolved_1"], function (_export, _context) {
+  "use strict";
+
+  var _reporterNs, _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, _decorator, Component, Button, director, PassInf, user, _dec, _class2, _crd, ccclass, property, reStart;
+
+  function _reportPossibleCrUseOfPassInf(extras) {
+    _reporterNs.report("PassInf", "./PassInf", _context.meta, extras);
+  }
+
+  return {
+    setters: [function (_unresolved_) {
+      _reporterNs = _unresolved_;
+    }, function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+      Button = _cc.Button;
+      director = _cc.director;
+    }, function (_unresolved_2) {
+      PassInf = _unresolved_2.PassInf;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "6f9fcq+6SBEU5XGIVG24QBt", "reStart", undefined);
+
+      __checkObsolete__(['_decorator', 'Component', 'Node', 'Button', 'director']);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+      user = class user {
+        constructor() {
+          this.Account = void 0;
+          this.Password = void 0;
+          this.save = void 0;
+          this.difficulty = void 0;
+          this.d1time1 = 0;
+          this.d1time2 = 0;
+          this.d1time3 = 0;
+          this.d1time = 0;
+          this.d2time1 = 0;
+          this.d2time2 = 0;
+          this.d2time3 = 0;
+          this.d2time = 0;
+        }
+
+      };
+
+      _export("reStart", reStart = (_dec = ccclass('reStart'), _dec(_class2 = class reStart extends Component {
+        start() {
+          this.node.on(Button.EventType.CLICK, this.restart, this);
+        }
+
+        update(deltaTime) {}
+
+        restart() {
+          let Account = director.getScene().getChildByName('PassNode').getComponent(_crd && PassInf === void 0 ? (_reportPossibleCrUseOfPassInf({
+            error: Error()
+          }), PassInf) : PassInf).CurrentUser.Account;
+          let RegSet = Object.assign(new Array(), JSON.parse(localStorage.getItem('RegSet')));
+
+          for (const item of RegSet) {
+            if (item.Account == Account) {
+              item.save = 0;
+              break;
+            }
+          }
+
+          let json = JSON.stringify(RegSet);
+          localStorage.setItem('RegSet', json);
+          director.loadScene('single1');
+        }
+
+      }) || _class2));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=6cf21e81582232dd2446393c126b03c349a6ed17.js.map
---
+++ TK2/temp/programming/packer-driver/targets/preview/chunks/46/461e3d4116c4ccec0c7ec0986705cb4badad788e.js
@@ -0,0 +1,66 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, _decorator, Button, Component, instantiate, Prefab, _dec, _dec2, _class, _class2, _descriptor, _crd, ccclass, property, LB;
+
+  function _initializerDefineProperty(target, property, descriptor, context) { if (!descriptor) return; Object.defineProperty(target, property, { enumerable: descriptor.enumerable, configurable: descriptor.configurable, writable: descriptor.writable, value: descriptor.initializer ? descriptor.initializer.call(context) : void 0 }); }
+
+  function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) { var desc = {}; Object.keys(descriptor).forEach(function (key) { desc[key] = descriptor[key]; }); desc.enumerable = !!desc.enumerable; desc.configurable = !!desc.configurable; if ('value' in desc || desc.initializer) { desc.writable = true; } desc = decorators.slice().reverse().reduce(function (desc, decorator) { return decorator(target, property, desc) || desc; }, desc); if (context && desc.initializer !== void 0) { desc.value = desc.initializer ? desc.initializer.call(context) : void 0; desc.initializer = undefined; } if (desc.initializer === void 0) { Object.defineProperty(target, property, desc); desc = null; } return desc; }
+
+  function _initializerWarningHelper(descriptor, context) { throw new Error('Decorating class property failed. Please ensure that ' + 'transform-class-properties is enabled and runs after the decorators transform.'); }
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      _decorator = _cc._decorator;
+      Button = _cc.Button;
+      Component = _cc.Component;
+      instantiate = _cc.instantiate;
+      Prefab = _cc.Prefab;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "022adMaW0pF+7NqVnIsxNc2", "LB", undefined);
+
+      __checkObsolete__(['_decorator', 'Button', 'Component', 'Input', 'instantiate', 'Node', 'Prefab']);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("LB", LB = (_dec = ccclass('LB'), _dec2 = property(Prefab), _dec(_class = (_class2 = class LB extends Component {
+        constructor() {
+          super(...arguments);
+
+          _initializerDefineProperty(this, "SLB", _descriptor, this);
+        }
+
+        start() {
+          this.node.on(Button.EventType.CLICK, this.LeaderBoard, this);
+        }
+
+        update(deltaTime) {}
+
+        LeaderBoard() {
+          var Board = instantiate(this.SLB);
+          this.node.parent.addChild(Board);
+        }
+
+      }, (_descriptor = _applyDecoratedDescriptor(_class2.prototype, "SLB", [_dec2], {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        initializer: null
+      })), _class2)) || _class));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=461e3d4116c4ccec0c7ec0986705cb4badad788e.js.map
